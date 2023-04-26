8d5780a3e2a3f06ccdbe76939c8ad6ee35642eb5
ArturT <mr.arturio@gmail.com>
2023-04-25 22:47:14 -0400

    Fill up users-api.js
    first commit on new branch. fill up the fail with code using lighthBnB project as example.
    not sure we need password field, if so - need to add bcrypt

--- routes/users-api.js
+++ routes/users-api.js
@@ -8,6 +8,10 @@
 const express = require('express');
 const router  = express.Router();
 const userQueries = require('../db/queries/users');
+const db = require('../db/connection');
+
+// Define pool object using db module
+const pool = db;
 
 router.get('/', (req, res) => {
   userQueries.getUsers()
@@ -20,5 +24,81 @@ router.get('/', (req, res) => {
         .json({ error: err.message });
     });
 });
+
+// Create a new user
+router.post("/", (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+
+  database.addUser(user)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "error" });
+      }
+
+      req.session.userId = user.id;
+      res.send("🤗");
+    })
+    .catch((e) => res.send(e));
+});
+
+// Log a user in
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+
+  database.getUserWithEmail(pool, email).then((user) => {
+    if (!user) {
+      return res.send({ error: "no user with that id" });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.send({ error: "error" });
+    }
+
+    req.session.userId = user.id;
+    res.send({
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      },
+    });
+  });
+});
+
+// Log a user out
+router.post("/logout", (req, res) => {
+  req.session.userId = null;
+  res.send({});
+});
+
+// Return information about the current user (based on cookie value)
+router.get("/me", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ message: "not logged in" });
+  }
+
+  database.getUserWithId(userId)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no user with that id" });
+      }
+
+      res.send({
+        user: {
+          name: user.name,
+          email: user.email,
+          id: userId,
+        },
+      });
+    })
+    .catch((e) => res.send(e));
+});
+
+module.exports = router;
 
 module.exports = router;
