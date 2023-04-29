810c13926da7d3308752955630b8a68b96935a57
ArturT <mr.arturio@gmail.com>
2023-04-28 20:09:20 -0400

    create basic queries for users

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,4 +7,71 @@ const getUsers = () => {
     });
 };
 
-module.exports = { getUsers };
+//Get a single user from the database given their email.
+const getUserWithEmail = function (pool, email) {
+  return pool
+    .query(
+      `SELECT *
+       FROM users
+       WHERE email = $1`,
+      [email]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+//Get a single user from the database given their id.
+const getUserWithId = function (usersId) {
+  return pool
+    .query(
+      `SELECT *
+       FROM users
+       WHERE users.id = $1`,
+      [usersId]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
+//Add a new user to the database.
+const addUser = function (user) {
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password)
+       VALUES ($1, $2, $3)
+       RETURNING *`,
+      [user.name, user.email, user.password]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
+module.exports = {
+  getUsers,
+  getUserWithEmail,
+  getUserWithId,
+  addUser,
+ };
