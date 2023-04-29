ed6bb6e7b46df722ef48892ad3381b41177e490c
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-04-29 03:03:50 +0000

    Databse updated

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,12 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Alice@gmail.com','Alice','1234', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Rob@aol.com','Rob','robspassword', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Peter@morrey.com','Peter','peter', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Grace@fido.com','Grace','abcd', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Greg01@hunter.com','Greg','4567', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Anthony@grapes.com','Anthony','anthony1', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Mary@doodle.com','Mary','MARY', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Marissa@gmail.com','Marissa','asdf', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('David@uptown.com','David','lostone', CURRENT_TIMESTAMP, avatar);
-INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Howard@gmail.com','Howard','Howard24', CURRENT_TIMESTAMP, avatar);
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Alice@gmail.com','Alice','1234', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Rob@aol.com','Rob','robspassword', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Peter@morrey.com','Peter','peter', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Grace@fido.com','Grace','abcd', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Greg01@hunter.com','Greg','4567', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Anthony@grapes.com','Anthony','anthony1', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Mary@doodle.com','Mary','MARY', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Marissa@gmail.com','Marissa','asdf', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('David@uptown.com','David','lostone', CURRENT_TIMESTAMP, 'avatar');
+INSERT INTO users (email, username, password, created_at, avatar) VALUES ('Howard@gmail.com','Howard','Howard24', CURRENT_TIMESTAMP, 'avatar');
 
