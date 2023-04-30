063d7dfb56dc0de4c8c391ba5c116cda062b52ac
ArturT <mr.arturio@gmail.com>
2023-04-30 18:07:17 -0400

    add CHECK (price >= 0) to price, and UNIQUE to username

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS favortites CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255),
   created_at TIMESTAMP,
   avatar TEXT
@@ -18,10 +18,10 @@ CREATE TABLE products (
   seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
-  price INTEGER NOT NULL,
+  price INTEGER NOT NULL CHECK (price >= 0),
   category VARCHAR(255) NOT NULL,
   created_at TIMESTAMP,
-  photo TEXT,
+  photo TEXT NOT NULL,
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
