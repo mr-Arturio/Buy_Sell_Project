df826aa89e0a0e8df49b226ae9fd6acc92720b84
ArturT <mr.arturio@gmail.com>
2023-04-30 21:47:58 -0400

    Get all favorites for a given user using JOIN

--- db/queries/favorites.js
+++ db/queries/favorites.js
@@ -1,41 +1,36 @@
 const pool = require('../connection');
 
-//get all favourites from one buyer
-const getFavorites = (buyer) => {
+// Get all favorites for a given user
+const getFavorites = (userId) => {
   return pool
-  .query(
-    'SELECT * FROM favorites WHERE users_id = $1',
-    [buyer]
+    .query(
+      'SELECT f.id AS favorite_id, f.user_id, p.* FROM favorites f JOIN products p ON f.product_id = p.id WHERE f.user_id = $1',
+      [userId]
     )
-    .then(data => {
-      return data.rows[0];
-    });
+    .then(data => data.rows);
 };
 
-//add to favourites
-const addFavorite = (products_id, users_id) => {
+// Add a new favorite for a given user
+const addFavorite = (productId, userId) => {
   return pool
-  .query(
-    'INSERT INTO favorites (products_id, users_id) VALUES ($1, $2) RETURNING *;',
-    [products_id, users_id]
+    .query(
+      'INSERT INTO favorites (product_id, user_id) VALUES ($1, $2) RETURNING *',
+      [productId, userId]
     )
-    .then(data => {
-      return data.rows[0];
-    });
+    .then(data => data.rows[0]);
 };
 
-
-//delete favourites
-const deleteFavorite = (id) => {
+// Remove a favorite for a given user
+const removeFavorite = (userId, favoriteId) => {
   return pool
-  .query(
-    'DELETE FROM favorites WHERE id = $1',
-    [id]
-    )
-  };
+    .query(
+      'DELETE FROM favorites WHERE user_id = $1 AND id = $2',
+      [userId, favoriteId]
+    );
+};
 
-  module.exports = {
-    deleteFavorite,
-    addFavorite,
-    getFavorites
-  };
+module.exports = {
+  getFavorites,
+  addFavorite,
+  removeFavorite
+};
