451e8df903e34283101c757080a2f7e13f7b975f
Daniel Soares <danielsoares01@outlook.com>
2023-05-03 20:11:03 -0700

    product-view page can now display an itemp from the database

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ const usersRoutes = require('./routes/users');
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
-app.use('/api/products', productApiRoutes);
+// app.use('/api/products', productApiRoutes);
+app.use('/products', productApiRoutes);
 app.use('/favorites', favoritesRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
@@ -71,9 +72,9 @@ app.get('/post-product', (req, res) => {
   res.render('post-product');
 });
 
-app.get('/product-view', (req, res) => {
-  res.render('product-view');
-});
+// app.get('/product-view', (req, res) => {
+//   res.render('product-view');
+// });
 
 app.listen(PORT, () => {
   console.log(`PlayNation is alive on port ${PORT}`);
