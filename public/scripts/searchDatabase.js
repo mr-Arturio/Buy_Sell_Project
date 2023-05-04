de1c7e6cdfc369ef078c08620546fac3733817b6
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-04 19:35:06 +0000

    search database .js file

--- public/scripts/searchDatabase.js
+++ public/scripts/searchDatabase.js
@@ -1,3 +1,21 @@
-function searchDatabse() {
-  
-}
+
+
+
+
+
+ 
+
+ $('submit-form').on('submit', (event) => {
+    event.preventDefault(); // prevent defualt form submission
+  const data = $('#myInput').val(); //get the search query from input feild on website
+    $.ajax({
+      type: 'GET',
+      url: '/search',
+      data: data,
+      success: function() {
+
+      }
+    })
+    })
+
+
