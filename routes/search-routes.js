340a19505c76119424cd37dd2d779f0169103f80
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-04 19:37:06 +0000

    Search route

---
+++ routes/search-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const searchQuery = require ('../db/queries/search');
+
+router.get('/', (req, res) => {
+  const query = req.query.name 
+  searchQuery.searchDatabse(query)
+  .then (results => {
+    res.json(results)
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).send("An error occored while searching the database")
+  })
+})
+
+module.exports = router;
