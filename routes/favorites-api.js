260fda7006cab0e349bf084986731c55287de702
ArturT <mr.arturio@gmail.com>
2023-05-06 22:00:47 -0400

    work in process, remove favorites

--- routes/favorites-api.js
+++ routes/favorites-api.js
@@ -33,16 +33,18 @@ router.post('/', (req, res, next) => {
     .catch(error => next(error));
 });
 
-// Remove a favorite for a given user
-router.delete('/:userId/:favoriteId', (req, res, next) => {
-  const userId = req.params.userId;
+// Remove a favorite
+router.delete('/', (req, res, next) => {
   const favoriteId = req.params.favoriteId;
 
-  favoritesQueries.removeFavorite(userId, favoriteId)
+  favoritesQueries.removeFavorite(favoriteId)
     .then(() => {
       res.sendStatus(204);
+
     })
     .catch(error => next(error));
 });
+
+
 
 module.exports = router;
